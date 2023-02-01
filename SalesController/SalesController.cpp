ffca919a81278504c53ec56d9168ac8d5bcfc05d
JulioYap <a20203861@pucp.edu.pe>
2023-02-01 16:21:48 -0500

    New Controller

--- SalesController/SalesController.cpp
+++ SalesController/SalesController.cpp
@@ -2,3 +2,23 @@
 
 #include "SalesController.h"
 
+int SalesController::Controller::AddProduct(Product^ product)
+{
+    productList->Add(product);
+    
+    return product->getId();
+}
+
+Product^ SalesController::Controller::ReadProductById(int productId)
+{
+    for (int i = 0; i < productList->Count; i++) // productList->Count shows amount of products
+        if (productList[i]->getId() == productId)   return productList[i];
+
+    // Instead
+    return nullptr;  // Return that means there's no it
+}
+
+List<Product^>^ SalesController::Controller::ReadAllProudcts()
+{
+    return productList;
+}
--- SalesController/SalesController.h
+++ SalesController/SalesController.h
@@ -1,12 +1,21 @@
-// Description: SalesController : Event Manager c:
+// Description: SalesController : Event Manager c:with business operations
 
 #pragma once
-
-using namespace System;
+using namespace System;	// Fro String
+using namespace System::Collections::Generic;		// For List
+using namespace SalesModel;
 
 namespace SalesController {
-	public ref class Class1
+	public ref class Controller
 	{
-		// TODO: Agregue aquí los métodos de esta clase.
+		private: 
+			static List<Product^>^ productList;							// STATIC means that's initializated just once :O
+																		// List is a new variable type similar to ARRAY without a defined space of memory :=O
+			// CRUD operations: C (Create), R(Read), U(Update/Refresh), D(Delete) 
+
+		public:
+			static int AddProduct(Product^ product);
+			static Product^ ReadProductById(int productId);			// Just one
+			static List<Product^>^ ReadAllProudcts();				// All of them
 	};
 }
