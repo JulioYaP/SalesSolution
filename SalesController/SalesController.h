cfc7aaed324a220356bb1bd472855f58b45710cd
JulioYap <a20203861@pucp.edu.pe>
2023-02-01 12:39:13 -0500

    Agregar archivos de proyecto.

---
+++ SalesController/SalesController.h
@@ -0,0 +1,12 @@
+// Description: SalesController : Event Manager
+
+#pragma once
+
+using namespace System;
+
+namespace SalesController {
+	public ref class Class1
+	{
+		// TODO: Agregue aquí los métodos de esta clase.
+	};
+}
